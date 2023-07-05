49eebd1dee8da734fd4d9c6e8929dddbb1c98f46
Arturo Perez <dash_arturo@hotmail.com>
2023-07-04 19:37:36 -0600

    PUT actualizamos un estudiante y agregamos logica de negocio antes de actualizar un estudiante

--- src/main/java/springMysql/demo/student/StudentService.java
+++ src/main/java/springMysql/demo/student/StudentService.java
@@ -1,10 +1,12 @@
 package springMysql.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,22 @@ if (!exits){
     throw new IllegalStateException("student with id " + studentId + "does not exits");
 }
 studenRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studenRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException( "student with id " + studentId + "does not exits"));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studenRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
